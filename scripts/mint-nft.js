f7f65cde7eb02399ec836617c1f918eded8d418e
mark-fernandez <mark.fernandez@swivel.work>
2022-10-10 23:47:59 -0700

    Added functionality to mint nft on the Goerli Eth Testnet

---
+++ scripts/mint-nft.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+const ethers = require('ethers');
+
+// Get Alchemy API URL from .env
+const API_KEY = process.env.API_KEY;
+
+// Define an Alchemy provider
+const provider = new ethers.providers.AlchemyProvider('goerli', API_KEY);
+
+const contract = require("../artifacts/contracts/ExampleNFT.sol/ExampleNFT.json");
+// console.log(JSON.stringify(contract.abi));
+
+// Create a signer
+const privateKey = process.env.PRIVATE_KEY;
+const signer = new ethers.Wallet(privateKey, provider);
+
+// Create a contract ABI and address
+const abi = contract.abi;
+const contractAddress = "0xFF7487eFa99aDdbf1f4E37fEbc0E1DC81D430Ff1";
+
+// Create a contract instance object
+const contractInstance = new ethers.Contract(contractAddress, abi, signer);
+
+// Get the NFT Metadata IPFS URL
+const tokenURI = "https://gateway.pinata.cloud/ipfs/QmaVK7Ry6PnRTmsXbTXpUWEMzA7MJEdtYGcWkVZ1CoXLCN"
+
+// Mint an NFT
+const mintNFT = async () => {
+    let nftTxn = await contractInstance.mintNFT(signer.address, tokenURI);
+    await nftTxn.wait();
+    console.log(`NFT Minted! Check it out at https://goerli.etherscan.io/tx/${nftTxn.hash}`);
+}
+
+mintNFT()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
