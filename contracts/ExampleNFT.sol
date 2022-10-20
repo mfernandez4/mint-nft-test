ea8cc3ef91b4850c07f669d7b592423ec94556dc
mark-fernandez <mark.fernandez@swivel.work>
2022-10-20 07:31:50 -0700

    adjusted the code and did a test run for the pinata storage. It worked.

--- contracts/ExampleNFT.sol
+++ contracts/ExampleNFT.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
--- scripts/store-asset-pinata.js
+++ scripts/store-asset-pinata.js
@@ -2,13 +2,15 @@ require('dotenv').config();
 const key = process.env.REACT_APP_PINATA_KEY;
 const secret = process.env.REACT_APP_PINATA_SECRET;
 const axios = require('axios');
+const FormData = require('form-data');
+const fs = require('fs');
 
 /**
  * @desc Pinata API call to pin JSON metadata to IPFS
  * @param {metadata} metadata - metadata to be uploaded and pinned to pinata
  * @returns A promise that resolves to the IPFS URL of the metadata
  */
-export const pinJSONToIPFS = async(metadata) => {
+const pinJSONToIPFS = async(metadata) => {
     const url = `https://api.pinata.cloud/pinning/pinJSONToIPFS`;
     return axios
         .post(url, metadata, {
@@ -33,10 +35,10 @@ export const pinJSONToIPFS = async(metadata) => {
         });
 };
 
-export const pinFileToIPFS = async(file) => {
+const pinFileToIPFS = async(file) => {
     const url = `https://api.pinata.cloud/pinning/pinFileToIPFS`;
     const data = new FormData();
-    data.append('file', file);
+    data.append('file', fs.createReadStream(file));
     return axios
         .post(url, data, {
             headers: {
@@ -60,3 +62,70 @@ export const pinFileToIPFS = async(file) => {
            
         });
 }
+
+
+async function main() {
+
+    // TEST CODE
+    
+    // create the image URI by uploading the image to Pinata
+    const imageURI = await pinFileToIPFS('assets/ExampleNFT.png');
+    console.log("Image asset stored on Pinata and IPFS with URL:", imageURI.pinataUrl)
+    
+    //make metadata
+    /**
+     * @desc Creates the metadata for the NFT
+     * @param {string} name - name of the NFT
+     * @param {string} image - URI for the image of the NFT
+     * @param {string} description - description of the NFT
+     * @param {string} external_url - external URL for the NFT
+     * @param {object} attributes - attributes of the NFT
+     */
+    const metadata = {};
+    metadata.name = 'ExampleNFT';
+    metadata.image = imageURI;
+    metadata.description = 'Example NFT to deploy to the Polygon Mumbai Test Network';
+    metadata.external_url = "https://www.swivelmeta.io"
+    metadata.attributes = [
+        {
+            "trait_type": "Team",
+            "value": "Swivel Meta"
+        },
+        {
+            "display_type": "boost_number",
+            "trait_type": "Coolness Factor",
+            "value": 20
+        },
+        {
+            "trait_type": "Speed",
+            "value": 100
+        },
+        {
+            "trait_type": "Jump",
+            "value": 20
+        },
+        {
+        "display_type": "number",
+        "trait_type": "Generation",
+        "value": 1
+        }
+    ];
+
+    const tokenURI = await pinJSONToIPFS(metadata)
+    .then(function (response) {
+        return response.pinataUrl
+    })
+    .catch(function (error) {
+        console.log(error)
+        return error.message
+    });
+    console.log("Metadata stored on Pinata and IPFS with URL:", tokenURI)
+}
+
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
